e788df75f0bbd09f405e3f6da37e54b7000c4e47
Mont <99560044+iotak0@users.noreply.github.com>
2024-04-03 21:25:55 +0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -48,7 +48,7 @@ setInterval(function() {
             let comments=json.list;
             for (const key in comments) {
                     const comment = comments[key];
-                    if (comment.userId==679||comment.userId==215240||comment.userId==39226||comment.userId==212031||comment.userId==55||comment.userId==8837||comment.userId==11||comment.userId==1) {
+                    if (comment.userId==27358||comment.userId==679||comment.userId==215240||comment.userId==39226||comment.userId==212031||comment.userId==55||comment.userId==8837||comment.userId==11||comment.userId==1) {
                         console.log(`If found a banded user @${comment.username}`);
                         try {
                             deletePost(comment); 
